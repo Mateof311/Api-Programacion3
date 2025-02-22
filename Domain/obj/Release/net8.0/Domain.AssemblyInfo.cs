43be1aa39b333ad6c49b16e4f8ade43f22048e32
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2025-02-22 16:58:02 -0300

    AddDocs

--- Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d98a295fb26e4a9f186fc62c55dadbecd21ec38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3242e6ec720cf30989db22accaca3af326d952d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
